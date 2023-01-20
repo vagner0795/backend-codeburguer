8a9c1badeb58c1ad6437cf6c8ec0748202e9e1b5
vagner0795 <vagner-se@hotmail.com>
2023-01-20 10:53:13 -0300

    my first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["standard", "prettier"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    camelcase: "off",
+    "prettier/prettier": "error",
+  },
+  "prettier/prettier": [
+    "error",
+    {
+      endOfLine: "auto",
+    },
+  ],
+};
